01af08a3b16f686522aa45d77e90816e5e42c2b6
wuyu <wqiq77@163.com>
2022-04-24 16:58:27 +0800

    l-text 组件 step 1

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,19 @@
 import { createStore } from 'vuex'
 import templates, { TemplateProps } from './modules/template'
 import user, { UserProps } from './modules/user'
+import editor, { EditorProps } from './modules/editor'
 
 export interface GlobalDataProps {
     user: UserProps
     templates: TemplateProps[]
+    editor: EditorProps
 }
 
 const store = createStore<GlobalDataProps>({
     modules: {
         user,
-        templates
+        templates,
+        editor
     },
 })
 
---
+++ src/store/modules/editor.ts
@@ -0,0 +1,34 @@
+import { v4 as uuidv4 } from 'uuid'
+import { Module } from 'vuex'
+import { GlobalDataProps } from '..'
+
+export interface EditorProps {
+    // 供中间编辑器渲染的数组
+    components: ComponentData[]
+    // 当前编辑的哪个元素 uuid
+    currentElement: string
+}
+
+interface ComponentData {
+    props: { [key: string]: any }
+    // uuid v4
+    id: string
+    // 业务组件名称 l-text, l-image 等
+    name: string
+}
+
+const testComponents: ComponentData[] = [
+    { id: uuidv4(), name: 'l-text', props: { text: 'hello', fontSize: '20px' } },
+    { id: uuidv4(), name: 'l-text', props: { text: 'hello2', fontSize: '10px' } },
+    { id: uuidv4(), name: 'l-text', props: { text: 'hello3', fontSize: '15px' } },
+]
+
+const editor: Module<EditorProps, GlobalDataProps> = {
+    namespaced: true,
+    state: {
+        components: testComponents,
+        currentElement: ''
+    }
+}
+
+export default editor
