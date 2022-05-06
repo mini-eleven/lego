dcd27251a242212699f041754967171af0ebe170
wuyu <wqiq77@163.com>
2022-05-06 14:48:04 +0800

    获取组件相关信息并展示

--- src/propsMap.ts
+++ src/propsMap.ts
@@ -2,7 +2,13 @@ import { TextComponentProps } from "./defaultProps"
 
 export interface PropToForm {
     component: string
+    subComponent?: string
     value?: string
+    extraProps?: { [key: string]: any }
+    text?: string
+    options?: { text: string; value: any }[]
+    initalTransform?: (v: any) => any
+    valueProp?: string
 }
 
 export type PropsToForms = {
@@ -11,6 +17,40 @@ export type PropsToForms = {
 
 export const mapPropsToForms: PropsToForms = {
     text: {
-        component: 'a-input'
+        component: 'a-input',
+        text: '文本'
+    },
+    fontSize: {
+        component: 'a-input-number',
+        text: '字号',
+        initalTransform: (v: string) => parseInt(v)
+    },
+    lineHeight: {
+        component: 'a-slider',
+        extraProps: { min: 0, max: 3, step: 0.1 },
+        text: '行高',
+        initalTransform: (v: string) => parseFloat(v)
+    },
+    textAlign: {
+        component: 'a-radio-group',
+        subComponent: 'a-radio-button',
+        text: '对齐',
+        options: [
+            { text: '左', value: 'left' },
+            { text: '中', value: 'center' },
+            { text: '右', value: 'right' }
+        ]
+    },
+    fontFamily: {
+        component: 'a-select',
+        subComponent: 'a-select-option',
+        text: '字体',
+        options: [
+            { text: '无', value: '' },
+            { text: '宋体', value: '"Simsun","STSong"' },
+            { text: '黑体', value: '"SimHei","STHeiti"' },
+            { text: '楷体', value: '"KaiTi","STKaiti"' },
+            { text: '仿宋', value: '"FangSong", "STFangsong" ' }
+        ]
     }
-}
+}                
--- src/store/modules/editor.ts
+++ src/store/modules/editor.ts
@@ -18,7 +18,7 @@ export interface ComponentData {
 }
 
 const testComponents: ComponentData[] = [
-    { id: uuidv4(), name: 'l-text', props: { text: 'hello', fontSize: '20px', color: 'red' } },
+    { id: uuidv4(), name: 'l-text', props: { text: 'hello', fontSize: '20px', color: 'red', lineHeight: 1, textAlign: 'left', fontFamily: '' } },
     { id: uuidv4(), name: 'l-text', props: { text: 'hello2', fontSize: '10px' } },
     { id: uuidv4(), name: 'l-text', props: { text: 'hello3', fontSize: '15px', actionType: 'url', url: 'https://www.baidu.com/' } },
 ]
