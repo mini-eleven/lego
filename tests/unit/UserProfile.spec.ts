a7c892a12965030335b7214d31e3abd176184037
wuyu <wqiq77@163.com>
2022-05-08 11:08:36 +0800

    mock vue-router

--- tests/unit/UserProfile.spec.ts
+++ tests/unit/UserProfile.spec.ts
@@ -1,10 +1,24 @@
 import { mount, shallowMount, VueWrapper } from '@vue/test-utils'
+import { message } from 'ant-design-vue'
 import UserProfile from '@/components/UserProfile.vue'
+import store from '@/store'
 
 let wrapper: VueWrapper<any>
-jest.mock('ant-design-vue')
-jest.mock('vuex')
-jest.mock('vue-router')
+jest.mock('ant-design-vue', () => ({
+    message: {
+        success: jest.fn()
+    }
+}))
+// jest.mock('vuex')
+// jest.mock('vue-router')
+
+const mockedRoutes: string[] = []
+jest.mock('vue-router', () => ({
+    useRouter: () => ({
+        push: (url: string) => mockedRoutes.push(url)
+    })
+}))
+
 const mockComponent = {
     template: '<div><slot></slot></div>'
 }
@@ -22,21 +36,28 @@ const globalComponent = {
 describe('UserProfile Component', () => {
 
     beforeAll(() => {
+        jest.useFakeTimers()
         // 单一实例 所有的case都共用同一个wrapper
         wrapper = mount(UserProfile, {
             props: {
                 user: { isLogin: false }
             },
             global: {
-                components: globalComponent
+                components: globalComponent,
+                provide: {
+                    store
+                }
             }
         })
 
     })
 
-    it('should render login button when login is false', () => {
+    it('should render login button when login is false', async () => {
         console.log(wrapper.html())
         expect(wrapper.get('div').text()).toBe('登录')
+        await wrapper.get('div').trigger('click')
+        expect(message.success).toHaveBeenCalled()
+        expect(store.state.user.userName).toBe('wuyu')
     })
 
     it('should render username when login is true', async () => {
@@ -48,7 +69,16 @@ describe('UserProfile Component', () => {
         expect(wrapper.find('.user-profile-dropdown').exists()).toBeTruthy()
     })
 
+    it('should call logout and show message, call router.push after timeout', async () => {
+        await wrapper.get('.user-profile-dropdown div').trigger('click')
+        expect(store.state.user.isLogin).toBeFalsy()
+        expect(message.success).toHaveBeenCalledTimes(1)
+        jest.runAllTimers()
+        expect(mockedRoutes).toEqual(['/'])
+    })
+
     afterEach(() => {
+        (message as jest.Mocked<typeof message>).success.mockReset()
     })
 })
 
